0207c3ee1eb02db18cf45b53f9b581699f8e27a6
neuro.SYS <neuro.sys@neurosys.gg>
2026-06-29 15:51:43 +0200

    - add missing download links for "hello world!"

--- data/releases.php
+++ data/releases.php
@@ -18,7 +18,10 @@ return [
         '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay; encrypted-media" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A2340758756%3Fsecret_token%3Ds-D3cfG0dzbeB&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/neuro-sys" title="neuro.SYS" target="_blank" style="color: #cccccc; text-decoration: none;">neuro.SYS</a> · <a href="https://soundcloud.com/neuro-sys/hello-world/s-D3cfG0dzbeB" title="hello world!" target="_blank" style="color: #cccccc; text-decoration: none;">hello world!</a></div>',
         'https://my.hidrive.com/api/sharelink/download?id=PFGaSOmtM',
         new Collection(Format::class)->add(
-            new Format(ReleaseFormat::FLAC, 'https://my.hidrive.com/api/sharelink/download?id=ebiFGBt52')
+            new Format(ReleaseFormat::FLAC, 'https://my.hidrive.com/api/sharelink/download?id=ebiFGBt52'),
+            new Format(ReleaseFormat::WAV, 'https://my.hidrive.com/api/sharelink/download?id=QX98AVCDz'),
+            new Format(ReleaseFormat::MP3, 'https://my.hidrive.com/api/sharelink/download?id=GATvNadI8'),
+            new Format(ReleaseFormat::STEMS, 'https://my.hidrive.com/api/sharelink/download?id=O6PtraldH'),
         )
     )
 ];
